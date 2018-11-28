b9c8478c2c3bd14d07790f3c49d7cc1dafd099dd
Jae-Hyeong Yang <jhyang12345@naver.com>
2018-11-28 13:38:27 +0900

    Done with task 2

--- task_2.py
+++ task_2.py
@@ -1,4 +1,8 @@
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import time
+
 import numpy as np
 import pandas as pd
 
@@ -6,9 +10,17 @@ def main():
     lottery_df = pd.read_csv("lottery.csv")
     valid_columns = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth']
     input_data = np.array(lottery_df[valid_columns[:3]])
-    print(input_data)
     kmeans = KMeans(n_clusters=3, random_state=0).fit(input_data)
-    print(kmeans.labels_)
+    fig = plt.figure(1, figsize=(4, 3))
+    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+    labels = kmeans.labels_
+    ax.w_xaxis.set_ticklabels([])
+    ax.w_yaxis.set_ticklabels([])
+    ax.w_zaxis.set_ticklabels([])
+    ax.scatter(input_data[:,0], input_data[:,1], input_data[:,2],
+        c=labels.astype(np.float), edgecolor='k')
+    fig.show()
+    plt.show()
     print(kmeans)
 
 
